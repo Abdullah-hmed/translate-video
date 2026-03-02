293197c8f6cd6153af4b4b610f1d603cd95855f4
Abdullah-hmed <abdullahwyi@gmail.com>
2026-03-02 11:22:10 +0500

    Implemented pocket tts translation into pipeline

--- asr.py
+++ asr.py
@@ -1,6 +1,7 @@
 from faster_whisper import WhisperModel
 from utils import convert_to_wav
-from translate import quick_translate
+from translate import quick_translate, mtranslate
+from tts import pocket_tts
 import argparse
 from pathlib import Path
 
@@ -16,9 +17,9 @@ def transcribe_audio(audio_path):
     # Concatenate all segments into a single string
     full_transcription = " ".join(segment.text for segment in segments)
 
-    Path('temp_audio.wav').unlink(missing_ok=True)
+    # Path('temp_audio.wav').unlink(missing_ok=True)
 
-    return full_transcription
+    return output_audio_path, full_transcription
 
 if __name__ == "__main__":
 
@@ -26,7 +27,13 @@ if __name__ == "__main__":
     parser.add_argument("audio_path", help="Path to the audio or video file")
     args = parser.parse_args()
 
-    transcription = transcribe_audio(args.audio_path)
+    output_audio_path, transcription = transcribe_audio(args.audio_path)
     print(f"Transcription: {transcription}")
 
-    print(f"Translated: {quick_translate(transcription, target='en')}")
+    translated_transcription = quick_translate(transcription, target='en')
+
+    print(f"Translated: {translated_transcription}")
+
+    pocket_tts(output_audio_path, translated_transcription, filename="source/translated_output.wav")
+    
+    Path('temp_audio.wav').unlink(missing_ok=True)
