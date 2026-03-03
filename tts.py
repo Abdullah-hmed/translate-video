908bd76b9456e3c0c2413d40e6d9604fdcbf10f7
Abdullah-hmed <abdullahwyi@gmail.com>
2026-03-03 07:13:57 +0500

    chopping input audio for optimal voice cloning

--- tts.py
+++ tts.py
@@ -1,11 +1,23 @@
 from pocket_tts import TTSModel
 import scipy.io.wavfile
 import os
+import soundfile as sf
+import librosa
 import argparse
+
+def trim_audio(input_path, output_path="source/trimmed_ref.wav", max_duration=12):
+    audio, sr = librosa.load(input_path, sr=None)
+    trimmed = audio[:int(max_duration * sr)]
+    sf.write(output_path, trimmed, sr)
+    return output_path
+
 
 def pocket_tts(input_audio, text, filename="pocket_output.wav"):    
     tts_model = TTSModel.load_model()
-    voice_state = tts_model.get_state_for_audio_prompt(input_audio)
+
+    trimmed_ref = trim_audio(input_audio)
+
+    voice_state = tts_model.get_state_for_audio_prompt(trimmed_ref)
     audio = tts_model.generate_audio(voice_state, text)
     # Audio is a 1D torch tensor containing PCM data.
     scipy.io.wavfile.write(filename, tts_model.sample_rate, audio.numpy())
